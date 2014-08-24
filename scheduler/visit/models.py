b7161b917879dc202bd2635c6d7b4a4592cf253a
Dougal Scott <Dougal.Scott@gmail.com>
2014-08-24 17:14:28 +1000

    Flake'd

--- scheduler/visit/models.py
+++ scheduler/visit/models.py
@@ -5,60 +5,64 @@ from django.db import models
 
 from client.models import Client, Notes, Day, inGap
 
+
 ################################################################################
 ################################################################################
 ################################################################################
 class Visit(models.Model):
-    client=models.ForeignKey(Client)
-    good=models.BooleanField(default=True)
-    date=models.ForeignKey(Day)
-    note=models.ForeignKey(Notes, null=True, blank=True)
+    client = models.ForeignKey(Client)
+    good = models.BooleanField(default=True)
+    date = models.ForeignKey(Day)
+    note = models.ForeignKey(Notes, null=True, blank=True)
 
     def __str__(self):
         return "Visit %s on %s" % (self.client, self.date)
 
     class Meta:
-        unique_together=(("client", "date"))
+        unique_together = (("client", "date"))
+
 
 ################################################################################
 def newVisit(client, d):
-    v=Visit(client=client, date=Day.objects.get(date=d))
+    v = Visit(client=client, date=Day.objects.get(date=d))
     v.save()
-    day=Day.objects.get_or_create(date=d, defaults={'date':d})[0]
-    day.unfilled-=client.duration
+    day = Day.objects.get_or_create(date=d, defaults={'date': d})[0]
+    day.unfilled -= client.duration
     if day.unfilled:
-        day.unfilled-=1 # Time to have lunch, travel, etc
+        day.unfilled -= 1  # Time to have lunch, travel, etc
     day.save()
     return v
 
+
 ################################################################################
 def makeVisits(client, startDate, endDate):
-    msgs=[]
-    d=startDate
-    d-=datetime.timedelta(days=1)
+    msgs = []
+    d = startDate
+    d -= datetime.timedelta(days=1)
     sys.stderr.write("Making visists for %s (%s)\n" % (client.name, client.duration))
-    lastdate=None
-    while d<endDate:
-        d+=datetime.timedelta(days=1)
-        day=Day.objects.get_or_create(date=d, defaults={'date':d})[0]
+    lastdate = None
+    while d < endDate:
+        d += datetime.timedelta(days=1)
+        day = Day.objects.get_or_create(date=d, defaults={'date': d})[0]
         if inGap(d):
             continue
         if day.isWeekend():
             continue
         if not client.goodDay(d):
             continue
-        if lastdate and d-lastdate<datetime.timedelta(days=7*client.regularity):
+        if lastdate and d-lastdate < datetime.timedelta(days=7*client.regularity):
             continue
         if day.canfit(client.duration):
-            v=newVisit(client, d)
-            if lastdate and d-lastdate>datetime.timedelta(days=7*client.regularity):
-                v.good=False
+            v = newVisit(client, d)
+            if lastdate and d-lastdate > datetime.timedelta(days=7*client.regularity):
+                v.good = False
                 v.save()
                 msgs.append("Visit on %s - %s days since last once (meant to be %s days)" % (day, d-lastdate, datetime.timedelta(days=7*client.regularity)))
             else:
                 msgs.append("Visit on %s\n" % day)
-            lastdate=v.date.date
+            lastdate = v.date.date
     return msgs
+
 
 ################################################################################
 def clearVisits():
@@ -67,4 +71,4 @@ def clearVisits():
     for d in Day.objects.all():
         d.delete()
 
-#EOF
+# EOF
