53ae5ac5b7351cba0b0dc27c86c3dcd4bfe6b5fe
Dougal Scott <Dougal.Scott@gmail.com>
2014-09-04 22:46:11 +1000

    Added statics to URL

--- scheduler/scheduler/urls.py
+++ scheduler/scheduler/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 from .views import index
 admin.autodiscover()
 
@@ -12,3 +15,5 @@ urlpatterns = patterns(
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
