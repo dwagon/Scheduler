8cc3933416eee8d38432676743455e05cbf632f7
Dougal Scott <Dougal.Scott@gmail.com>
2014-09-09 22:39:39 +1000

    Added authentication

--- scheduler/scheduler/urls.py
+++ scheduler/scheduler/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import login, logout
+from django.conf import settings
 
 from .views import index, exportData
 
@@ -16,6 +18,8 @@ urlpatterns = patterns(
     url(r'^visit/', include('visit.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$',  login, {'template_name': 'base/login.html'}, name='login'),
+    url(r'^accounts/logout/$', logout, {'next_page': '/'}, name='logout'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- scheduler/scheduler/views.py
+++ scheduler/scheduler/views.py
@@ -1,19 +1,22 @@
 import csv
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 from client.models import Client
 from visit.models import Visit
 
 
 ################################################################################
+@login_required
 def index(request):
     template_name = "base/index.html"
-    context = {}
-    return render(request, template_name, context)
+    return render(request, template_name, context_instance=RequestContext(request))
 
 
 ################################################################################
+@login_required
 def exportData(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="schedule.csv"'
@@ -25,5 +28,13 @@ def exportData(request):
         for visit in Visit.objects.filter(client=client):
             writer.writerow(['', visit.date, visit.note])
     return response
+
+
+################################################################################
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
 
 # EOF
