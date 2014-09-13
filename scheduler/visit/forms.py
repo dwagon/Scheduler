f2a56c6162923e5e5ce901882c6a5b461348ec97
Dougal Scott <Dougal.Scott@gmail.com>
2014-09-13 18:07:46 +1000

    Allow users to edit the goodness

--- scheduler/visit/forms.py
+++ scheduler/visit/forms.py
@@ -6,7 +6,7 @@ from .models import Visit
 class VisitForm(forms.ModelForm):
     class Meta:
         model = Visit
-        exclude = ('client', 'good')
+        exclude = ('client',)
         widgets = {
             'note': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
         }
