61498368b778df603f498d6228394fe21e414940
Dougal Scott <Dougal.Scott@gmail.com>
2014-09-14 11:47:44 +1000

    Improving client update form

--- scheduler/client/forms.py
+++ scheduler/client/forms.py
@@ -13,7 +13,17 @@ class ClientForm(forms.ModelForm):
         model = Client
         fields = '__all__'
         widgets = {
-            'note': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
+            'note': forms.Textarea(attrs={'cols': 50, 'rows': 5, 'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ClientForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['regularity'].widget.attrs.update({'class': 'form-control'})
+        self.fields['dayofweek'].widget.attrs.update({'class': 'form-control'})
+        self.fields['duration'].widget.attrs.update({'class': 'form-control'})
+        self.fields['flexible'].widget.attrs.update({'class': 'form-control'})
+        self.fields['startdate'].widget.attrs.update({'class': 'form-control'})
+        self.fields['enddate'].widget.attrs.update({'class': 'form-control', 'type': 'date'})
 
 # EOF
