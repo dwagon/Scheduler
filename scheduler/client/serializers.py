cee551cb54b3dd8846641e0d0257b572f75a8f4e
Dougal Scott <Dougal.Scott@gmail.com>
2014-09-20 11:51:56 +1000

    Fixed the fields being serialized

--- scheduler/client/serializers.py
+++ scheduler/client/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ('name', 'regularity', 'dayofweek', 'duration', 'startdate', 'enddate')
+        fields = ('name', 'regularity', 'dayofweek', 'duration', 'startdate', 'enddate', 'note')
 
 
 # EOF
