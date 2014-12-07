f20be7fa47eb7f9b486cc07fad155c30beec4576
Dougal Scott <Dougal.Scott@gmail.com>
2014-12-07 21:49:14 +1100

    Remove debug

--- scheduler/visit/forms.py
+++ scheduler/visit/forms.py
@@ -7,7 +7,6 @@ class VisitForm(forms.ModelForm):
 
     class Meta:
         model = Visit
-        exclude = ('client',)
         widgets = {
             'note': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
         }
@@ -24,15 +23,11 @@ class VisitNewForm(forms.ModelForm):
         exclude = ('attn',)
         widgets = {
             'note': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
-            'client': forms.HiddenInput()
         }
 
     def __init__(self, *args, **kwargs):
         super(VisitNewForm, self).__init__(*args, **kwargs)
         self.fields['date'].widget.attrs.update({'class': 'datepicker'})
-        import sys
-        sys.stderr.write("locals=%s\n" % locals())
-        sys.stderr.write("self.fields=%s\n" % self.fields)
         self.fields['client'].value = kwargs['initial']['client']
 
 # EOF
